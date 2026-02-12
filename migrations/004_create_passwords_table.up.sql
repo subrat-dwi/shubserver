77e5c4fe3e10e9dc7133cd0eba16b96f1654a6e1
subrat-dwi <chubukov789@gmail.com>
2026-02-12 23:32:35 +0530

    fix: correct index definition for name and username in passwords table

--- migrations/004_create_passwords_table.up.sql
+++ migrations/004_create_passwords_table.up.sql
@@ -32,7 +32,7 @@ ON passwords(user_id);
 -- Trigram index for service name search within user's passwords
 CREATE INDEX IF NOT EXISTS idx_passwords_name_username_trgm 
 ON passwords 
-USING gin (name, username gin_trgm_ops);
+USING gin (name gin_trgm_ops);
 
 -- Comments for documentation
 COMMENT ON TABLE passwords IS 'Encrypted password storage - server stores ciphertext only, never decrypts';
